eb14a959bda05c16f3ffeb8c6c30fd5864b1aa80
shreyank.bansod <shreyank.bansod@cionlabs.com>
2024-09-09 17:47:13 +0530

    Setup Navigation and Designed Layouts!

---
+++ app/src/main/java/com/example/roomappstevdzasan/fragments/list/ListFragment.kt
@@ -0,0 +1,36 @@
+package com.example.roomappstevdzasan.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.roomappstevdzasan.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+//=====// Below code gives Error: `unresolved reference: floatingActionButton`
+//        view.floatingActionButton.setOnClickListener {
+//            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+//        }
+
+// =====// Solution: Find the FloatingActionButton by its ID
+        val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+        floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
+    }
+
+}
